903aacfb975a984426b947afc045758cb6e01943
Tarion <ProjectZulu@outlook.com>
2013-05-04 16:41:44 -0300

    Major Version++

--- src/jas/common/DefaultProps.java
+++ src/jas/common/DefaultProps.java
@@ -3,16 +3,16 @@ package jas.common;
 public class DefaultProps {
     public static final String MODID = "JustAnotherSpawner";
     public static final String MODNAME = "Just Another Spawner";
-    public static final String VERSION = "0.5.2";
+    public static final String VERSION = "0.6.0";
     public static final String MODDIR = "/JustAnotherSpawner/";
     public static final String GLOBALSETTINGSDIR = MODDIR;
     public static final String WORLDSETTINGSDIR = MODDIR + "WorldSettings/";
 
     public static final String ENTITYSUBDIR = "ModEntitySettings/";
     public static final String ENTITYTYPESUBDIR = "LivingTypeSettings/";
-    
+
     public static final String GUIDIR = "/mods/jas/gui/";
-    
+
     public static final String DELIMETER = "-";
     public static final String defaultChannel = "JAS_Channel";
 }
