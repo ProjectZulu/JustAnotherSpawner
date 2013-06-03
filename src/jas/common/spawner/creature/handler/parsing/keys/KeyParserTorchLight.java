a29988ea2ff44da61ded4131c76209949d8ef295
Tarion <ProjectZulu@outlook.com>
2013-06-03 14:58:51 -0300

    Light and Torchlight tags are now invertable.

--- src/jas/common/spawner/creature/handler/parsing/keys/KeyParserTorchLight.java
+++ src/jas/common/spawner/creature/handler/parsing/keys/KeyParserTorchLight.java
@@ -1,49 +1,22 @@
 package jas.common.spawner.creature.handler.parsing.keys;
 
-import jas.common.spawner.creature.handler.parsing.OptionalParser;
 import jas.common.spawner.creature.handler.parsing.TypeValuePair;
-import jas.common.spawner.creature.handler.parsing.settings.OptionalSettings.Operand;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
 
-public class KeyParserTorchLight extends KeyParserBase {
+public class KeyParserTorchLight extends KeyParserRange {
 
     public KeyParserTorchLight(Key key) {
-        super(key, false, KeyType.CHAINABLE);
+        super(key);
     }
 
     @Override
-    public boolean parseChainable(String parseable, ArrayList<TypeValuePair> parsedChainable,
-            ArrayList<Operand> operandvalue) {
-        String[] pieces = parseable.split(",");
-        Operand operand = getOperand(pieces);
-
-        TypeValuePair typeValue = new TypeValuePair(key, OptionalParser.parseDoubleInteger(pieces,
-                new int[] { 16, -1 }, key.key));
-
-        if (typeValue.getValue() != null) {
-            parsedChainable.add(typeValue);
-            operandvalue.add(operand);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean parseValue(String parseable, HashMap<String, Object> valueCache) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean isValidLocation(World world, EntityLiving entity, int xCoord, int yCoord, int zCoord,
-            TypeValuePair typeValuePair, HashMap<String, Object> valueCache) {
-        int[] lightLevels = (int[]) typeValuePair.getValue();
-        int lightLevel = world.getSavedLightValue(EnumSkyBlock.Block, xCoord, yCoord, zCoord);
-        return lightLevel > lightLevels[1] || lightLevel < lightLevels[0];
+    int getCurrent(World world, EntityLiving entity, int xCoord, int yCoord, int zCoord, TypeValuePair typeValuePair,
+            HashMap<String, Object> valueCache) {
+        return world.getSavedLightValue(EnumSkyBlock.Block, xCoord, yCoord, zCoord);
     }
 }
