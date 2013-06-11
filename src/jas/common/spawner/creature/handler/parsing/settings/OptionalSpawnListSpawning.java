88e03bf12193e291faefce6133bbc98c613d26ef
Tarion <ProjectZulu@outlook.com>
2013-06-11 13:57:32 -0300

    Setup SpawnList optional with valid property keys

--- src/jas/common/spawner/creature/handler/parsing/settings/OptionalSpawnListSpawning.java
+++ src/jas/common/spawner/creature/handler/parsing/settings/OptionalSpawnListSpawning.java
@@ -7,6 +7,9 @@ import java.util.EnumSet;
 public class OptionalSpawnListSpawning extends OptionalSettingsBase {
 
     public OptionalSpawnListSpawning(String parseableString) {
-        super(parseableString, EnumSet.of(Key.spawn));
+        super(parseableString.replace("}", ""), EnumSet.of(Key.spawn, Key.light, Key.block, Key.blockRange,
+                Key.blockFoot, Key.spawnRange, Key.sky, Key.minSpawnHeight, Key.maxSpawnHeight, Key.liquid, Key.opaque,
+                Key.normal, Key.solidSide, Key.difficulty, Key.torchLight, Key.ground, Key.top, Key.fill, Key.modspawn,
+                Key.origin, Key.players));
     }
 }
