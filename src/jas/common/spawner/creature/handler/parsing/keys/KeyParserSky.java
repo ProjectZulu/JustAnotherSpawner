535fcfc3fb962305ff6a9b951d7b8fcdfc76ac0b
Tarion <ProjectZulu@outlook.com>
2013-09-01 21:17:23 -0300

    Refactor KeyParserGround and KeyParserSky to extend KeyParserBoolean. Should be easier to maintain.

--- src/jas/common/spawner/creature/handler/parsing/keys/KeyParserSky.java
+++ src/jas/common/spawner/creature/handler/parsing/keys/KeyParserSky.java
@@ -1,50 +1,16 @@
 package jas.common.spawner.creature.handler.parsing.keys;
 
-import jas.common.JASLog;
-import jas.common.spawner.creature.handler.parsing.TypeValuePair;
-import jas.common.spawner.creature.handler.parsing.settings.OptionalSettings.Operand;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.world.World;
 
-public class KeyParserSky extends KeyParserBase {
+public class KeyParserSky extends KeyParserBoolean {
 
     public KeyParserSky(Key key) {
-        super(key, true, KeyType.CHAINABLE);
+        super(key);
     }
 
     @Override
-    public boolean parseChainable(String parseable, ArrayList<TypeValuePair> parsedChainable,
-            ArrayList<Operand> operandvalue) {
-        String[] pieces = parseable.split(",");
-        Operand operand = parseOperand(pieces);
-        if (pieces.length == 1) {
-            parsedChainable.add(new TypeValuePair(key, isInverted(parseable)));
-            operandvalue.add(operand);
-            return true;
-        } else {
-            JASLog.severe("Error Parsing Needs Sky parameter. Invalid Argument Length.");
-            return false;
-        }
-    }
-
-    @Override
-    public boolean parseValue(String parseable, HashMap<String, Object> valueCache) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean isValidLocation(World world, EntityLiving entity, int xCoord, int yCoord, int zCoord,
-            TypeValuePair typeValuePair, HashMap<String, Object> valueCache) {
-        boolean isInverted = (Boolean) typeValuePair.getValue();
-        boolean canSeeSky = canBlockSeeTheSky(world, xCoord, yCoord, zCoord);
-        return isInverted ? canSeeSky : !canSeeSky;
-    }
-
-    protected boolean canBlockSeeTheSky(World world, int xCoord, int yCoord, int zCoord) {
+    public boolean getValue(EntityLiving entity, World world, int xCoord, int yCoord, int zCoord) {
         return world.canBlockSeeTheSky(xCoord, yCoord, zCoord);
     }
 }
