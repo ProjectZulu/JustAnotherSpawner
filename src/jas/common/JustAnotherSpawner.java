b4a02c5788f92a2dd40eab0103ac77223d994c45
Tarion <ProjectZulu@outlook.com>
2013-09-03 12:02:08 -0300

    Remove depreciated annotations

--- src/jas/common/JustAnotherSpawner.java
+++ src/jas/common/JustAnotherSpawner.java
@@ -16,11 +16,8 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.world.WorldEvent;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.Mod.PostInit;
-import cpw.mods.fml.common.Mod.PreInit;
-import cpw.mods.fml.common.Mod.ServerStarting;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
@@ -63,7 +60,7 @@ public class JustAnotherSpawner {
         return worldSettings;
     }
 
-    @PreInit
+    @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         modConfigDirectoryFile = event.getModConfigurationDirectory();
         globalSettings = new GlobalSettings(modConfigDirectoryFile);
@@ -72,13 +69,13 @@ public class JustAnotherSpawner {
         // proxy.registerKeyBinding();
     }
 
-    @Init
+    @EventHandler
     public void load(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new EntityDespawner());
         NetworkRegistry.instance().registerGuiHandler(modInstance, new GuiHandler());
     }
 
-    @PostInit
+    @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         BiomeDictionary.registerAllBiomes();
         biomeBlacklist = new BiomeBlacklist(modConfigDirectoryFile);
@@ -88,7 +85,7 @@ public class JustAnotherSpawner {
         MinecraftForge.EVENT_BUS.post(new CompatibilityRegistrationEvent(new CompatabilityRegister()));
     }
 
-    @ServerStarting
+    @EventHandler
     public void serverStart(FMLServerStartingEvent event) {
         worldSettings = new WorldSettings(modConfigDirectoryFile, event.getServer().worldServers[0], importedSpawnList);
         event.registerServerCommand(new CommandJAS());
