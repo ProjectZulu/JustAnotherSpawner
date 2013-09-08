9bdbadaeb9f9f90bfbe9163a9e812894f58408d5
Tarion <ProjectZulu@outlook.com>
2013-09-08 14:28:59 -0300

    Revision++

--- src/jas/common/DefaultProps.java
+++ src/jas/common/DefaultProps.java
@@ -3,7 +3,7 @@ package jas.common;
 public class DefaultProps {
     public static final String MODID = "JustAnotherSpawner";
     public static final String MODNAME = "Just Another Spawner";
-    public static final String VERSION = "0.11.3";
+    public static final String VERSION = "0.11.4";
     public static final String MODDIR = "/JustAnotherSpawner/";
     public static final String GLOBALSETTINGSDIR = MODDIR;
     public static final String WORLDSETTINGSDIR = MODDIR + "WorldSettings/";
