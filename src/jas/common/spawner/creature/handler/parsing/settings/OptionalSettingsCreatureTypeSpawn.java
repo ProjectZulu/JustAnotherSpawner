a33a0008bf184082c9e10c4f95d78b2742cd09da
Tarion <ProjectZulu@outlook.com>
2013-09-27 16:06:50 -0300

    Add Dim tag. Accepts a single integer value and compares it to current dimension ID. Is invertible and chainable.

--- src/jas/common/spawner/creature/handler/parsing/settings/OptionalSettingsCreatureTypeSpawn.java
+++ src/jas/common/spawner/creature/handler/parsing/settings/OptionalSettingsCreatureTypeSpawn.java
@@ -10,6 +10,6 @@ public class OptionalSettingsCreatureTypeSpawn extends OptionalSettingsBase {
         super(parseableString.replace("}", ""), EnumSet.of(Key.spawn, Key.light, Key.block, Key.blockRange,
                 Key.blockFoot, Key.spawnRange, Key.sky, Key.minSpawnHeight, Key.maxSpawnHeight, Key.liquid, Key.opaque,
                 Key.normal, Key.solidSide, Key.difficulty, Key.torchLight, Key.ground, Key.top, Key.fill, Key.origin,
-                Key.players, Key.entities, Key.random, Key.location));
+                Key.players, Key.entities, Key.random, Key.location, Key.dimension));
     }
 }
