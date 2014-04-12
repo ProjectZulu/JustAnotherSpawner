245e46b83d9e2b8e6bbadf6b6ea307033d35e9a0
Tarion <ProjectZulu@outlook.com>
2014-04-12 15:06:07 -0300

    Add FILE_VERSION to temporarySaveSettings.cfg

--- src/main/java/jas/common/SavedFolderConfiguration.java
+++ src/main/java/jas/common/SavedFolderConfiguration.java
@@ -5,6 +5,8 @@ import java.io.File;
 import com.google.gson.annotations.SerializedName;
 
 public class SavedFolderConfiguration {
+    @SerializedName("FILE_VERSION")
+    public String fileVersion;
     public final String _IMPORTANT = "# DO NOT TOUCH. Internally used to remember how the Configuration file was actually saved.";
     /* Marks how the Entity CFG settings should be saved */
     @SerializedName("Use_Universal_Entity_CFG")
@@ -16,6 +18,7 @@ public class SavedFolderConfiguration {
 
     public SavedFolderConfiguration(boolean universalDirectory) {
         this.universalDirectory = universalDirectory;
+        this.fileVersion = "1.0";
     }
 
     public static File getFile(File configDirectory, String saveName) {
