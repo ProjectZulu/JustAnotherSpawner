e1bcdb3802d75d9464b5f961bd459c15b638f092
crudedragos <ProjectZulu@Outlook.com>
2014-08-18 19:00:30 -0400

    Add commands to add/remove/update CreatureTypes Currently Update functions as replace.

---
+++ src/main/java/jas/common/modification/ModAddCreatureType.java
@@ -0,0 +1,36 @@
+package jas.common.modification;
+
+import jas.common.spawner.biome.group.BiomeGroupRegistry;
+import jas.common.spawner.creature.type.CreatureType;
+import jas.common.spawner.creature.type.CreatureTypeBuilder;
+import jas.common.spawner.creature.type.CreatureTypeRegistry;
+
+import java.util.ArrayList;
+
+import com.google.common.base.Optional;
+
+public class ModAddCreatureType extends BaseModification {
+
+	private CreatureTypeBuilder builder;
+
+	public ModAddCreatureType(String typeID) {
+		this(typeID, 1, 10);
+	}
+
+	public ModAddCreatureType(String typeID, int spawnRate, int maxNumberOfCreature) {
+		builder = new CreatureTypeBuilder(typeID, spawnRate, maxNumberOfCreature);
+	}
+
+	public ModAddCreatureType(String typeID, int spawnRate, int maxNumberOfCreature, float chunkSpawnChance,
+			String spawnMedium, String tags) {
+		this(typeID, spawnRate, maxNumberOfCreature);
+		builder.withChanceToChunkSpawn(chunkSpawnChance);
+		builder.setRawMedium(spawnMedium);
+		builder.withOptionalParameters(tags);
+	}
+
+	@Override
+	public void applyModification(CreatureTypeRegistry registry) {
+		registry.addCreatureType(builder);
+	}
+}
