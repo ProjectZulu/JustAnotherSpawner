929e16b9a8c16f5e3e171cfee902a97b3c2f96f4
crudedragos <ProjectZulu@Outlook.com>
2014-08-23 13:22:28 -0400

    Add Side effects handling to ModRemoveBiomeGroup: Remove spawnlists associated with the biomegroup.

--- src/main/java/jas/common/modification/ModRemoveBiomeGroup.java
+++ src/main/java/jas/common/modification/ModRemoveBiomeGroup.java
@@ -1,7 +1,18 @@
 package jas.common.modification;
 
+import java.util.Collection;
+
+import jas.common.ImportedSpawnList;
+import jas.common.JustAnotherSpawner;
 import jas.common.spawner.biome.group.BiomeGroupRegistry;
 import jas.common.spawner.biome.group.BiomeGroupRegistry.BiomeGroup;
+import jas.common.spawner.creature.entry.BiomeSpawnListRegistry;
+import jas.common.spawner.creature.entry.SpawnListEntryBuilder;
+import jas.common.spawner.creature.handler.LivingGroupRegistry;
+import jas.common.spawner.creature.handler.LivingHandler;
+import jas.common.spawner.creature.handler.LivingHandlerRegistry;
+import jas.common.spawner.creature.handler.LivingGroupRegistry.LivingGroup;
+import jas.common.spawner.creature.type.CreatureTypeRegistry;
 
 public class ModRemoveBiomeGroup extends BaseModification {
 
@@ -14,5 +25,20 @@ public class ModRemoveBiomeGroup extends BaseModification {
 	@Override
 	public void applyModification(BiomeGroupRegistry registry) {
 		registry.removeBiomeGroup(groupName);
+	}
+
+	@Override
+	public void applyModification(BiomeSpawnListRegistry registry) {
+		LivingHandlerRegistry livingHandlerRegistry = registry.livingHandlerRegistry;
+		BiomeGroupRegistry biomeGroupRegistry = registry.biomeGroupRegistry;
+
+		/* For all LivingGroups (that are not CreatureType NONE) */
+		Collection<LivingHandler> livingHandlers = livingHandlerRegistry.getLivingHandlers();
+		for (LivingHandler handler : livingHandlers) {
+			if (handler.creatureTypeID.equalsIgnoreCase(CreatureTypeRegistry.NONE)) {
+				continue;
+			}
+			registry.removeSpawnListEntry(handler.groupID, groupName);
+		}
 	}
 }
